d4c7dbc1a230fa0b989aaefb3c18c1b114109471
Ben Gordon <benjamintheepic@gmail.com>
2019-10-22 14:14:43 -0400

    save

--- points-complete.py
+++ points-complete.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+import random
+import tkinter
+import math
+
+num = 5
+current_move = 1
+p1_score = 0
+p2_score = 0
+
 adjacencies = [
     [0, 1, 1, 1, 1],
     [1, 0, 1, 1, 1],
@@ -7,9 +16,125 @@ adjacencies = [
     [1, 1, 1, 1, 0]
 ]
 
+
 moves = []
 
 triangles = []
 
+def generate_triangles(n):
+    global triangles
+    global num
+    global adjacencies
+    num = n
+    triangles = []
+    l = []
+    iterate_l = [x for x in range(0, n)]
+    for v1 in iterate_l:
+        v2_l = iterate_l.copy()
+        v2_l.remove(v1)
+        for v2 in v2_l:
+            v3_l = iterate_l.copy()
+            v3_l.remove(v1)
+            v3_l.remove(v2)
+            for v3 in v3_l:
+                l.append([v1, v2, v3])
+
+    l2 = l.copy()
+    for x in l2:
+        x.sort()
+        
+    l3 = []
+    for x in l2:
+        if x not in l3:
+            l3.append(x)
+    for x in range(0, len(l3)):
+        l3[x].append(l3[x][0])
+    for x in l3:
+        new_data = []
+        for i in range(0, 3):
+            data = [x[i], x[i+1]]
+            data.sort()
+            data.append('a')
+            new_data.append(data)
+        triangles.append(new_data)
+    adjacencies = []
+    for i in range(0, n):
+        adjacencies.append([])
+        for j in range(0, n):
+            if i == j:
+                adjacencies[i].append(0)
+            else:
+                adjacencies[i].append(1)
+
+def check_triangles(move):
+    filled_triangles = []
+    global p1_score
+    global p2_score
+    for x in range(0, len(triangles)):
+        counter = 0
+        sign_counter = 0
+        for i in range(0, 3):
+            if triangles[x][i][2] != 'a':
+                counter += 1
+                if moves[triangles[x][i][2] - 1][2] == "+":
+                    sign_counter += 1
+        if counter == 3:
+            filled_triangles.append([max([i[2] for i in triangles[x]]), sign_counter])
+    print(filled_triangles)
+
+
+generate_triangles(int(input("Complete graph on how many vertices? > ")))
+window = tkinter.Tk()
+w = tkinter.Canvas(window, width=800, height=800) 
+w.pack()
+
+lines = {}
+for x in range(0, num):
+    lines[x] = {}
+for x in range(0, num):
+    w.create_oval(390+300*math.cos(2*math.pi*x/num), 390+300*math.sin(2*math.pi*x/num), 410+300*math.cos(2*math.pi*x/num), 410+300*math.sin(2*math.pi*x/num), fill='black')
+    w.create_text(400+350*math.cos(2*math.pi*x/num), 400+350*math.sin(2*math.pi*x/num),fill="black",font="Times 20 bold", text=f"{x}")
+for i in range(0, num):
+    for j in range(0, num):
+        lines[i][j] = w.create_line(400+300*math.cos(2*math.pi*i/num), 400+300*math.sin(2*math.pi*i/num), 400+300*math.cos(2*math.pi*j/num), 400+300*math.sin(2*math.pi*j/num), fill="blue", width=5)
+
+
+current_player = 1
+
 def move(v1, v2, sign, player):
-    
+    global moves
+    global triangles
+    global current_move
+    global adjacencies
+    global current_player
+    vertices = [v1, v2]
+    v1 = min(vertices)
+    v2 = max(vertices)
+    previous_edges_marked = [x[0:2] for x in moves]
+    if [v1, v2] not in previous_edges_marked and v1 != v2 and v1 < num and v2 < num:
+        moves.append([v1, v2, sign, player])
+        for tri in range(0, len(triangles)):
+            for edge in range(0, 3):
+                if triangles[tri][edge][0:2] == [v1, v2]:
+                    triangles[tri][edge][2] = current_move
+        adjacencies[v1][v2] = sign
+        adjacencies[v2][v1] = sign
+        current_move += 1
+        
+        w.delete(lines[v1][v2])
+        if sign == '-':
+            lines[v1][v2] = w.create_line(400+300*math.cos(2*math.pi*v1/num), 400+300*math.sin(2*math.pi*v1/num), 400+300*math.cos(2*math.pi*v2/num), 400+300*math.sin(2*math.pi*v2/num), fill="black", width=5)
+        if sign == '+':
+            lines[v1][v2] = w.create_line(400+300*math.cos(2*math.pi*v1/num), 400+300*math.sin(2*math.pi*v1/num), 400+300*math.cos(2*math.pi*v2/num), 400+300*math.sin(2*math.pi*v2/num), fill="red", width=5)
+        
+        if current_player == 1:
+            current_player = 2
+        else:
+            current_player = 1
+    else: 
+        print("This edge has already been moved on")
+
+while True:
+    check_triangles(current_move)
+    move(int(input(f"Player {current_player}, select v1 > ")), int(input(f"Player {current_player}, select v2 > ")), input(f"Player {current_player}, select a sign (+ or -) > "), current_player)
+    print()
